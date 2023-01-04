405ca9ea35058752c4f71b7286ed6135c79d7b06
hashimoon <98980386+hashimoon@users.noreply.github.com>
2023-01-04 11:38:59 -0800

    coomit

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "fakewebservices_vpc" "primary_vpc" {
 }
 
 resource "fakewebservices_server" "servers" {
-  count = 3
+  count = 2
 
   name = "Server ${count.index + 1}"
   type = "t2.micro"
