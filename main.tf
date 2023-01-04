aba23c5b086192d684a83978278f863cb83b2dc8
hashimoon <98980386+hashimoon@users.noreply.github.com>
2023-01-04 11:48:44 -0800

    asdas

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "fakewebservices_vpc" "primary_vpc" {
 }
 
 resource "fakewebservices_server" "servers" {
-  count = 12
+  count = 118
 
   name = "Server ${count.index + 1}"
   type = "t2.micro"
